8427f45498cabd18b99a40eb1d19954344daea3b
Evan Larkin <evanlarkin@Evans-MacBook-Pro-2.local>
2023-03-09 19:46:13 -0500

    fetch sellers api

---
+++ api/models/seller.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, { DataTypes }) => {
+  return sequelize.define("seller", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    location: {
+      type: DataTypes.GEOMETRY,
+    },
+    createdAt: {
+      field: "created_at",
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      field: "updated_at",
+      type: DataTypes.DATE,
+    },
+  });
+};
